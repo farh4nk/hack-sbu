4edf8d3d7b09682b859970db85b6d314a24b1431
Farhan Khan <fk27179@gmail.com>
2025-11-07 21:31:42 -0500

    added analyze endpoint

--- backend/main.py
+++ backend/main.py
@@ -31,5 +31,31 @@ async def analyze(data: ImageInput):
     try:
         img_data = data.image.split(",")[1]
     except IndexError:
-        img_data
+        img_data = data.image
+    img_bytes = base64.b64decode(img_data)
+    img = Image.open(io.BytesIO(img_bytes))
 
+    results = model.predict(np.array(img))
+
+    labels = []
+
+    for box in results[0].boxes:
+        cls_id = int(box.cls)
+        label = results[0].names[cls_id]
+        labels.append(label)
+
+    if not labels:
+        summary = "No objects detected."
+
+    else:
+        unique = list(set(labels))
+        if len(unique) == 1:
+            summary = f"Detected one {unique[0]}"
+        else:
+            summary = f"Detected {', '.join(unique[:-1])}, and {unique[-1]}."
+
+    return {
+        "objects": labels,
+        "summary": summary,
+        "mode": data.mode
+    }
